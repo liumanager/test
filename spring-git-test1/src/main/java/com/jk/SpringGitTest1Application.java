abf4b95de540cd5c7314e786f3f68a9992870831
liumanager <953611136@qq.com>
2018-01-25 20:04:17 +0800

    第一次提交 有点紧张.

---
+++ spring-git-test1/src/main/java/com/jk/SpringGitTest1Application.java
@@ -0,0 +1,12 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitTest1Application.class, args);
+	}
+}
