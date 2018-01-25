391d77a124299633caf6131fa54bf7e60b01551b
liumanager <953611136@qq.com>
2018-01-25 20:47:08 +0800

    第二次修改 希望不紧张

---
+++ spring-git-test1/src/main/java/com/jk/controller/ConController.java
@@ -0,0 +1,10 @@
+package com.jk.controller;
+
+/**
+ * Created by Liubowen on 2018/1/25.
+ */
+public class ConController {
+    public static void main(String[] args) {
+        System.out.println("2123132321321");
+    }
+}
